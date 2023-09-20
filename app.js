fee097de13af1642dfdbfbdad55e807837e6096b
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2023-09-20 15:10:23 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,30 @@
+// <div class="parent">
+//     <div class="child">
+//         <h1></h1>
+//     </div>
+// </div>
+
+const headingNested = React.createElement('div', {}, 
+                        React.createElement('div', {}, 
+                            React.createElement('h1', {}, 'Im a H1 tag')
+                        )
+                    )
+
+// <div class="parent">
+//     <div class="child">
+//         <h1>Im a H1 tag</h1>
+//         <h2>Im a H2 tag</h2>
+//     </div>
+// </div>
+
+const headingNestedSibling = React.createElement('div', {}, 
+                        React.createElement('div', {}, 
+                            [React.createElement('h1', {}, 'Im a H1 tag'),React.createElement('h2', {}, 'Im a H2 tag')]
+                        )
+                    )
+
+const heading = React.createElement('h1', {}, 'Hello World from React...!!!')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(headingNestedSibling);
